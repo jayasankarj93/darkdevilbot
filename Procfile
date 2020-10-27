bb6aa9af93902e64c6e5b0ab52eb5bfd1ee31737
jayasankarj93 <68055360+jayasankarj93@users.noreply.github.com>
2020-10-28 03:28:04 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m darkdevilbot
+web: python3 -m darkdevilbot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE darkdevilbot
+:: Enables virtual env mode and then starts darkdevilbot
+env\scripts\activate.bat && py -m darkdevilbot
